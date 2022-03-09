d3bce58d2ada24968c366d6d8b6158547b1b0b48
Yuqing Li <liyuqing1220@gmail.com>
2022-03-09 10:29:32 +0100

    Update Convolution.m

--- Convolution.m
+++ Convolution.m
@@ -4,31 +4,27 @@
 clear all
 close all
 
-cd 'C:\Users\root\Documents\00 phd\measurement\201020 KEMAR HRTF AP\dry audio\audio files'
+cd 'C:\Users\root\Documents\00 phd\Database\dry audio\audio files'
 %%%% dry audio
-[input fs] = audioread('castanetcut_48.wav');
-input = 0.5*input;
-dis = 40;
-ori = 270;
+[input fs] = audioread('Cello melody2_v2_ch1_48k.wav');
+% input = 0.5*input;
+% dis = 40;
+% ori = 270;
 
 % for n = 1:72
 %     ori = 5*(n-1);
-%%% HRIR
-filepath = strcat('C:\Users\root\Documents\00 phd\measurement\201020 KEMAR HRTF AP\HRIRs\1203\',num2str(dis),'cm');
-cd(filepath);
-filename_l = strcat('HRIR_',num2str(dis),'_',num2str(ori),'_l.wav');
-filename_r = strcat('HRIR_',num2str(dis),'_',num2str(ori),'_r.wav');
-HRIR_l = audioread(filename_l);
-HRIR_r = audioread(filename_r);
+
+cd('C:\Users\root\Documents\00 phd\Database');
+filter_l = audioread('headphone_HD800_comp_KEMAR_Left.wav');
+filter_r = audioread('headphone_HD800_comp_KEMAR_Right.wav');
 
 %%%% convolution
-output = zeros(length(input)+length(HRIR_l)-1,2);
-output(:,1) = conv(input, HRIR_l);
-output(:,2) = conv(input, HRIR_r);
-% output = output/max(max(abs(output)));                           % normalization (does it influence localization)
+output = zeros(length(input)+length(filter_l)-1,2);
+output(:,1) = conv(input, filter_l);
+output(:,2) = conv(input, filter_r);
+output = output/max(max(abs(output)));                           % normalization (does it influence localization)
 
 %%%% signal output
-cd 'C:\Users\root\Documents\00 phd\measurement\201020 KEMAR HRTF AP\convolved audio\1207'
-outputname = strcat('1207_',num2str(dis),'_',num2str(ori),'.wav');
-audiowrite(outputname,output,fs);
+cd 'C:\Users\root\Documents\00 phd\renderer\WP1_stage3_code_original\HRTF_database_for_SOBR'
+audiowrite('compensated_input_cello.wav',output,fs);
 % end
