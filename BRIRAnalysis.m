24c93c3704097ccc971881d79678a508aa5b19c9
Yuqing Li <yuqingli.ac@gmail.com>
2024-08-08 17:42:05 +0200

    Update BRIRAnalysis.m

--- BRIRAnalysis.m
+++ BRIRAnalysis.m
@@ -1,6 +1,8 @@
 clear
 close all
 
+cd 'L:\Cloud\DataBackup\KEMARtrajectory'
+
 fs=44100;
 c=343;
 
@@ -17,9 +19,9 @@ for i=1:length(disV)
     IRmat(:,:,i,3)=importdata(fname_S7);
     IRmat(:,:,i,4)=importdata(fname_STV);
 end
-% IRmat=IRmat(1:2048,:,:,:);
+IRmat=IRmat(1:2048,:,:,:);
 
-Specmat=fft(IRmat,fs,1);
+Specmat=fft(IRmat);
 
 %%
 fAxis=[0:floor(length(Specmat)/2)-1]/length(Specmat)*fs;
@@ -27,16 +29,16 @@ Specmat_plot=2*Specmat([1:floor(length(Specmat)/2)],:,:,:);
 Specmat_plot(1,:,:,:)=Specmat_plot(1,:,:,:)/2;
 
 [X,Y]=meshgrid(fAxis,disV);
-[X1,Y1]=meshgrid([0:fs-1]/fs,disV);
+[X1,Y1]=meshgrid([0:length(IRmat)-1]/fs,disV);
 figname={'S4','S5','S7','STV'};
 for j = 1:4
     figure
     set(gcf,'Units','normalized');
-    set(gcf,'Position',[0.1 0.1 0.5 0.8]);
+    set(gcf,'Position',[0.1 0.1 0.3 0.6]);
     t=tiledlayout(2,1,"TileSpacing","compact");
     nexttile
     pc1=pcolor(X,Y,transpose(squeeze(mag2db(abs(Specmat_plot(:,1,:,j))))));colorbar;clim([-30 20]);
-    colormap('gray');
+%     colormap('gray');
     pc1.LineStyle="none";
     pc1.FaceColor="interp";
     set(gca,'Layer','top');
@@ -46,7 +48,7 @@ for j = 1:4
     title([figname(j) 'left']);
     nexttile
     pc2=pcolor(X,Y,transpose(squeeze(mag2db(abs(Specmat_plot(:,2,:,j))))));colorbar;clim([-30 20]);
-    colormap('gray');
+%     colormap('gray');
     pc2.LineStyle='none';
     pc2.FaceColor="interp";
     set(gca,'Layer','top');
@@ -61,7 +63,7 @@ for j = 1:4
     set(gcf,'Position',[0.1 0.1 0.3 0.6]);
     t=tiledlayout(2,1,"TileSpacing","compact");
     nexttile
-    pc3=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat(1:fs,1,:,j))))));colorbar;clim([-40 0]);
+    pc3=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat(:,1,:,j))))));colorbar;clim([-40 0]);
     oldcmap = colormap('gray');
     colormap( flipud(oldcmap) );
     pc3.LineStyle="none";
@@ -73,7 +75,7 @@ for j = 1:4
     xlim([0 0.05]);
     title([figname(j) 'left']);
     nexttile
-    pc4=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat(1:fs,2,:,j))))));colorbar;clim([-40 0]);
+    pc4=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat(:,2,:,j))))));colorbar;clim([-40 0]);
     colormap( flipud(oldcmap) );
     pc4.LineStyle='none';
     pc4.FaceColor="interp";
