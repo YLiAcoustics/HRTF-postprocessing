138da9d489d37fec6841b30e96ead24cab9720db
Yuqing Li <yuqingli.ac@gmail.com>
2024-10-23 17:37:41 +0200

    Update BRIRAnalysis.m

--- BRIRAnalysis.m
+++ BRIRAnalysis.m
@@ -5,7 +5,7 @@ cd 'L:\Cloud\DataBackup\KEMARtrajectory'
 
 fs=44100;
 c=343;
-traj = 2;
+traj = 1;
 
 if traj == 1
     disV=[0:0.1:3.5];
@@ -33,19 +33,57 @@ elseif traj==2
         IRmat(:,:,i,1)=importdata(fname_S4);
         IRmat(:,:,i,2)=importdata(fname_S5);
         IRmat(:,:,i,3)=importdata(fname_S7);
-        IRmat(:,:,i,4)=importdata(fname_STV);
+        IRmat(:,:,i,4)=importdata(fname_STV);       
     end
 end
-IRmat=IRmat(1:2048,:,:,:);
+IRmat=IRmat(1:44100,:,:,:);
+IRmat_L = squeeze(IRmat(:,1,:,:));
+IRmat_R = squeeze(IRmat(:,2,:,:));
+HanWin = hann(100);
+WinFun = ones(44100,1);
+WinFun(1:50) = HanWin(1:50);
+WinFun(44100-49:end) = HanWin(51:100);
+IRmat_L = IRmat_L.*WinFun;
+IRmat_R = IRmat_R.*WinFun;
 
-Specmat=fft(IRmat);
+Specmat_L=fft(IRmat_L);
+Specmat_R=fft(IRmat_R);
+
+%% spectral smoothing
+for i = 1:size(Specmat_L,2)
+    for j = 1:size(Specmat_R,3)
+        tempsmoothedL = smoothnew3(abs(squeeze(Specmat_L(:,i,j))),1/3);
+        Specmat_L_smoothed(:,i,j) = tempsmoothedL.'; 
+        tempsmoothedR= smoothnew3(abs(squeeze(Specmat_R(:,i,j))),1/3);
+        Specmat_R_smoothed(:,i,j) = tempsmoothedR.';
+    end
+end
 
 %%
-fAxis=[0:floor(length(Specmat)/2)-1]/length(Specmat)*fs;
-Specmat_plot=2*Specmat([1:floor(length(Specmat)/2)],:,:,:);
-Specmat_plot(1,:,:,:)=Specmat_plot(1,:,:,:)/2;
+fAxis=[0:floor(length(Specmat_L)/2)-1]/length(Specmat_L)*fs;
+Specmat_L_plot=2*Specmat_L_smoothed([1:floor(length(Specmat_L_smoothed)/2)],:,:);
+Specmat_L_plot(1,:,:)=Specmat_L_plot(1,:,:)/2;
+Specmat_R_plot=2*Specmat_R_smoothed([1:floor(length(Specmat_R_smoothed)/2)],:,:);
+Specmat_R_plot(1,:,:)=Specmat_R_plot(1,:,:)/2;
 
-[X,Y]=meshgrid(fAxis,disV);
+% fAxis=[0:floor(length(Specmat_L)/2)-1]/length(Specmat_L)*fs;
+% Specmat_L_plot=2*Specmat_L([1:floor(length(Specmat_L)/2)],:,:);
+% Specmat_L_plot(1,:,:)=Specmat_L_plot(1,:,:)/2;
+% Specmat_R_plot=2*Specmat_R([1:floor(length(Specmat_R)/2)],:,:);
+% Specmat_R_plot(1,:,:)=Specmat_R_plot(1,:,:)/2;
+% 
+% %% spectral smoothing
+% for i = 1:size(Specmat_L,2)
+%     for j = 1:size(Specmat_R,3)
+%         tempsmoothedL = smoothnew3(squeeze(Specmat_L_plot(:,i,j)),1/3);
+%         Specmat_L_smoothed(:,i,j) = tempsmoothedL.'; 
+%         tempsmoothedR= smoothnew3(squeeze(Specmat_R_plot(:,i,j)),1/3);
+%         Specmat_R_smoothed(:,i,j) = tempsmoothedR.';
+%     end
+% end
+
+%%
+[X,Y]=meshgrid(fAxis/1000,disV);
 [X1,Y1]=meshgrid([0:length(IRmat)-1]/fs,disV);
 figname={'S4','S5','S7','STV'};
 for j = 1:4
@@ -54,54 +92,62 @@ for j = 1:4
     set(gcf,'Position',[0.1 0.1 0.3 0.6]);
     t=tiledlayout(2,1,"TileSpacing","compact");
     nexttile
-    pc1=pcolor(X,Y,transpose(squeeze(mag2db(abs(Specmat_plot(:,1,:,j))))));cb=colorbar;clim([-20 20]);
-    cb.Label.String='Mag (dB)'
-%     colormap('gray');
+    pc1=pcolor(X,Y,transpose(squeeze(mag2db(abs(Specmat_L_plot(:,:,j))))));cb=colorbar;clim([-20 20]);
+    cb.Label.String='Mag (dB)';
+%     colormap(redblueTecplot);
     pc1.LineStyle="none";
     pc1.FaceColor="interp";
     set(gca,'Layer','top');
-    set(gca,'FontSize',14);
+    set(gca,'FontSize',12);
     set(gca,'LineWidth',1);
+    set(gca,'XScale','log');
     ylabel('Distance (m)');
+    xticks([0.1 1 10]);
+    xticklabels([0.1 1 10]);
+    xlim([50 22050]/1000);
     title([figname(j) 'left']);
     nexttile
-    pc2=pcolor(X,Y,transpose(squeeze(mag2db(abs(Specmat_plot(:,2,:,j))))));cb=colorbar;clim([-20 20]);
-    cb.Label.String='Mag (dB)'
-%     colormap('gray');
+    pc2=pcolor(X,Y,transpose(squeeze(mag2db(abs(Specmat_R_plot(:,:,j))))));cb=colorbar;clim([-20 20]);
+    cb.Label.String='Mag (dB)';
+%     colormap(redblueTecplot);
     pc2.LineStyle='none';
     pc2.FaceColor="interp";
     set(gca,'Layer','top');
-    set(gca,'FontSize',14);
+    set(gca,'FontSize',12);
     set(gca,'LineWidth',1);
-    xlabel('Freq (Hz)');
+    set(gca,'XScale','log');
+    xlim([50 22050]/1000);
+    xlabel('Freq (kHz)');
+    xticks([0.1 1 10]);
+    xticklabels([0.1 1 10]);
     ylabel('Distance (m)');
-    title([figname(j) ' right']);
+    title('right');
 
     figure
     set(gcf,'Units','normalized');
     set(gcf,'Position',[0.1 0.1 0.3 0.6]);
     t=tiledlayout(2,1,"TileSpacing","compact");
     nexttile
-    pc3=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat(:,1,:,j))))));colorbar;clim([-50 0]);
+    pc3=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat_L(:,:,j))))));colorbar;clim([-50 0]);
     oldcmap = colormap('gray');
 %     colormap( flipud(oldcmap) );
     colormap('gray');
     pc3.LineStyle="none";
     pc3.FaceColor="interp";
     set(gca,'Layer','top');
-    set(gca,'FontSize',14);
+    set(gca,'FontSize',12);
     set(gca,'LineWidth',1);
     ylabel('Distance (m)');
     xlim([0 0.05]);
     title([figname(j) 'left']);
     nexttile
-    pc4=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat(:,2,:,j))))));colorbar;clim([-50 0]);
+    pc4=pcolor(X1,Y1,transpose(squeeze(mag2db(abs(IRmat_R(:,:,j))))));colorbar;clim([-50 0]);
 %     colormap( flipud(oldcmap) );
     colormap('gray');
     pc4.LineStyle='none';
     pc4.FaceColor="interp";
     set(gca,'Layer','top');
-    set(gca,'FontSize',14);
+    set(gca,'FontSize',12);
     set(gca,'LineWidth',1);
     xlim([0 0.05]);
     xlabel('Time (s)');
